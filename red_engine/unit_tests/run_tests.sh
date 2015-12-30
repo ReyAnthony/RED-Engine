8bbf3c92e312d420813df52feedc8b8570969d4e
Anthony Rey <anthony.rey5@gmail.com>
2015-12-30 22:10:24 +0100

    Created keyboard engine unit tests

---
+++ red_engine/unit_tests/run_tests.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+g++ -std=gnu++1y engine_tests.cpp -o tests.out -lcppunit /usr/local/lib/red_engine/red_engine.a `pkg-config --cflags --libs allegro-5.0 allegro_acodec-5.0 allegro_audio-5.0 allegro_color-5.0 allegro_dialog-5.0 allegro_font-5.0 allegro_image-5.0 allegro_main-5.0 allegro_memfile-5.0 allegro_physfs-5.0 allegro_primitives-5.0 allegro_ttf-5.0`
+./tests.out
