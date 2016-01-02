0a3630b10d5f9bdf8e5a659d65e265c7358a1114
Anthony Rey <anthony.rey5@gmail.com>
2016-01-02 19:46:59 +0100

    Added copyright notices to the source

--- game/z_components/map.cpp
+++ game/z_components/map.cpp
@@ -1,6 +1,21 @@
-//
-// Created by Anthony REY on 20/12/2015.
-//
+/*
+	map.cpp is part of
+	RED-ENGINE - An Object-Oriented game engine based on Allegro5
+	Copyright (C) 2015 Anthony REY
+
+	This program is free software: you can redistribute it and/or modify
+			it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+			but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "map.hpp"
 
